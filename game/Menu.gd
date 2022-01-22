4d69d93c0242c9c3f4b53256aaa5d532043defab
Maxime CHARRON <maxime.charron@epitech.eu>
2022-01-22 15:12:38 +0100

    adding working menu, merging the two projects

---
+++ game/Menu.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Map.tscn")
+
+
+func _on_Params_pressed():
+	get_tree().change_scene("res://Options.tscn")
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
+
+func _input(event):
+	if event.is_action_pressed('ui_cancel'):
+		get_tree().quit()
---
+++ game/Options.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var music_enable = true
+var sfx_enable = true
+
+var main_volume = 50.0
+var musique_volume = 50.0
+var sfx_volume = 50.0
+
+var Vsync = false
